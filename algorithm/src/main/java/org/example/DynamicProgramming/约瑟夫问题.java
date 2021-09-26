a31ce17d2c42705670d3e7c45abbb5975ed8af93
zhmhbest <zhmhbest@gmail.com>
2021-09-26 20:08:05 +0800

    update

--- algorithm/src/main/java/org/example/DynamicProgramming/约瑟夫问题.java
+++ algorithm/src/main/java/org/example/DynamicProgramming/约瑟夫问题.java
@@ -1,4 +1,4 @@
-package org.example.Array;
+package org.example.DynamicProgramming;
 
 import org.example.EasyAssert;
 
---
+++ algorithm/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * 运行模板
+ */
+public class Main {
+    static int lastRemaining(int n, int k) {
+        int v = 0;
+        for (int i = 1; i <= n; i++) {
+            v = (v + k) % i;
+        }
+        return v;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        int n = scanner.nextInt();
+        System.out.println(lastRemaining(n, k));
+        // 2 3
+        // 2
+    }
+}
